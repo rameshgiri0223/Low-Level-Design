e758e05cac403c479879809ee434b7b41118f29d
Ramesh <102596577+rameshgiri0223@users.noreply.github.com>
2024-10-21 00:55:56 +0530

    Delete borrowedBook.java

--- borrowedBook.java
+++
@@ -1,9 +0,0 @@
-
-public class borrowedBook {
-
-    public static int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
-    }
-
-}
