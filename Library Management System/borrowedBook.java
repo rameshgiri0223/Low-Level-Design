3dc060968c05e1986198cb179e1cc4a5b32977a7
Ramesh <102596577+rameshgiri0223@users.noreply.github.com>
2024-10-21 00:53:56 +0530

    Add files via upload

---
+++ Library Management System/borrowedBook.java
@@ -0,0 +1,9 @@
+
+public class borrowedBook {
+
+    public static int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+}
